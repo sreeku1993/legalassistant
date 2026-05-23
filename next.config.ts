18c39fba4916eeb1ab53eae8f2c249c206671dfd
sreeku1993 <33524291+sreeku1993@users.noreply.github.com>
2026-05-23 20:20:52 +0530

    Enhance next.config.ts with outputFileTracingExcludes
    Added experimental outputFileTracingExcludes to optimize builds.

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,19 @@ const nextConfig: NextConfig = {
   serverExternalPackages: [
     "@xenova/transformers",
     "onnxruntime-node"
-  ]
+  ],
+
+  experimental: {
+    // Force Vercel to strip away heavy, unused operating system binaries
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/onnxruntime-node/bin/napi-v3/win32/**',
+        'node_modules/onnxruntime-node/bin/napi-v3/darwin/**',
+        'node_modules/onnxruntime-node/bin/napi-v3/linux/arm64/**', // Delete if not deploying to ARM
+        'node_modules/@xenova/transformers/deps/**',
+      ],
+    },
+  },
 };
 
 export default nextConfig;
